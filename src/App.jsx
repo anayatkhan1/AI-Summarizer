d4df44041a4c5a0d1838ce6d31ae81197936cfc4
anayatkhan1 <anayat0khan@gmail.com>
2023-07-11 21:50:43 +0530

    Aisummarizer

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Hero, Demo } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+        <div className="main">
+            <div className="gradient" />
+        </div>
+
+        <div className="app">
+            <Hero />
+            <Demo />
+        </div>
+    </main>
+  )
+}
+
+export default App
